b7c0e671ebd472ae2cadde64586ef5b0f17ec651
Kevin Higgins <shiburizu312@gmail.com>
2021-08-17 01:08:52 -0600

    revert to old accept_challenge to monitor errors

--- main.py
+++ main.py
@@ -126,16 +126,19 @@ class Lobby(db.Model):
         p1 = self.validate_id(target)
         p2 = self.validate_id(id)
         if p1 and p2:
-            #set player statuses
-            if p2.status != 'playing':
-                p2.status = 'playing'
-            #set IPs
-            if p2.ip != None and p1.ip == None:
-                p1.ip = p2.ip
-            db.session.add(p1)
-            db.session.commit()
-            self.accept_challenge(target,id) #swap players to do logic check
-            return gen_resp('OK','OK')
+            if p1.status != "playing" and p2.status != "playing":
+                p1.status = "playing"
+                p2.status = "playing"
+                if p1.ip != None and p2.ip == None:
+                    p2.ip = p1.ip
+                elif p2.ip != None and p1.ip == None:
+                    p1.ip = p2.ip
+                db.session.add(p1)
+                db.session.add(p2)
+                db.session.commit()
+                return gen_resp('OK','OK')
+            else:
+                return gen_resp('Already marked as playing.','OK')
         else:
             return gen_resp('Not in lobby.','FAIL')
 
