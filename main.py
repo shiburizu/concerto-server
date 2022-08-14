74eec6a753d9349550eb7384692416a56e9cb2c1
Shiburizu <shiburizu312@gmail.com>
2022-08-13 23:41:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -274,7 +274,7 @@ def version_check():
             if valid_name(name):
                 return gen_resp('OK','OK')
             else:
-                return gen_resp('Your name contains banned words. Please refer to concerto.shib.live to report false positives.','FAIL')
+                return gen_resp('Your name contains banned words. The server filter may overreach or cause unintended blocks. Please refer to the help section concerto.shib.live for guidance on reporting potential false positives.','FAIL')
         else:
             return gen_resp('UPDATE','FAIL')
     return gen_resp('No action found.','FAIL')
